766357a1edf219c614860f39b2ca4768db45c3d3
diepveetee <diepvt@mail.uc.edu>
2026-02-13 20:03:45 -0500

    Created FortuneTeller project. On step 3 of Document.

---
+++ src/FortuneTellerFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class FortuneTellerFrame extends JFrame {
+
+    public static void main(String[] args)
+    {
+        // The invokeLater method ensures the GUI is created on the
+        // Event Dispatch Thread (EDT) to avoid thread interference.
+
+        //SwingUtilities.invokeLater(() -> { ( ) = new FortuneTellerFrame();
+
+    }
+
+
+}
+
+
+
